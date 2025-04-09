94675e342f0782f1be5b89942608dd037fa97209
jessica joseph <jessicajoseph2019@yahoo.com>
2025-04-09 23:47:13 +0100

    fix: cors error

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,11 @@ app.use(
 dotenv.config();
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      process.env.BASEURLPROD,
+    ],
     credentials: true,
   })
 );
